d6f7a5dff73972aca221748b258504811882db1a
unknown <sk8695451>
2022-11-13 21:03:29 +0530

    start

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,8 @@ import React from 'react';
 
 const App = () => {
     return (
-        <h1></h1>
+        <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat, dicta enim voluptatum animi quis molestiae illum voluptas optio harum, molestias reprehenderit id eaque itaque placeat maiores nesciunt. Eveniet, non repellendus?</h1>
     );
 };
+
 export default App;
